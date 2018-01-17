8a1986436c97d14c340a31c1361129f504247856
Felix Krause <krausefx@gmail.com>
2018-01-17 15:21:28 -0500

    Fix initia clone of configs repo & auto delete broken one

--- services/config_data_sources/git_config_data_source.rb
+++ services/config_data_sources/git_config_data_source.rb
@@ -31,8 +31,15 @@ module FastlaneCI
     # Pulls the latest changes from remote repo
     def setup_repo
       if File.directory?(local_git_directory)
-        Dir.chdir(local_git_directory) do
-          FastlaneApp::CMD.run("git pull")
+        if File.directory?(File.join(local_git_directory, ".git"))
+          Dir.chdir(local_git_directory) do
+            FastlaneApp::CMD.run("git pull")
+          end
+        else
+          # directory exists, but no git directory
+          # clear the old directory and re-clone
+          FileUtils.rm_rf(local_git_directory)
+          setup_repo
         end
       else
         FastlaneApp::CMD.run("git clone", self.git_url, local_git_directory)
@@ -40,11 +47,11 @@ module FastlaneCI
     end
 
     # This is where we store the local git repo
+    # fastlane.ci will also delete this directory if it breaks
+    # and just re-clones. So make sure it's fine if it gets deleted
     def local_git_directory
       # TODO: fallback to use /tmp if we don't have the permission to write to this directory
-      local_git_directory = File.expand_path("~/.fastlane/ci/")
-      FileUtils.mkdir_p(local_git_directory) unless File.directory?(local_git_directory)
-      return local_git_directory
+      return File.expand_path("~/.fastlane/ci/")
     end
 
     private
