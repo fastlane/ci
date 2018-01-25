7756c8e13ef4223c82acecec59e77f04037d3710
Felix Krause <krausefx@gmail.com>
2018-01-25 15:04:16 -0500

    Migrate more code to work with new architecture

--- shared/models/project.rb
+++ shared/models/project.rb
@@ -29,7 +29,7 @@ module FastlaneCI
       # TODO: I assume we have to move this somewhere else?
       # TODO: Services::BUILD_SERVICE doesn't work as the file isn't included
       # TODO: ugh, I'm doing something wrong, I think?
-      json_folder_path = FastlaneCI::FastlaneApp::CONFIG_DATA_SOURCE.git_repo.path
+      json_folder_path = FastlaneCI::FastlaneApp::CONFIG_DATA_SOURCE.git_repo.git_config.local_repo_path
       build_service = FastlaneCI::BuildService.new(data_source: BuildDataSource.new(json_folder_path: json_folder_path))
       builds = build_service.list_builds(project: self)
 
