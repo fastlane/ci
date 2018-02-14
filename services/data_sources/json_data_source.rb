9f2a8489769e956d4c1b47f2d547732380c904e9
Jorge Revuelta <jrevuelta@usal.es>
2018-02-14 20:16:20 +0100

    Refactoring.
    - `JSONDataSource` now allows parameter injection and customizable
    post-creation behavior.

--- services/data_sources/json_data_source.rb
+++ services/data_sources/json_data_source.rb
@@ -1,14 +1,27 @@
 module FastlaneCI
   # define a simple initialization of instances which rely on a JSON file
   module JSONDataSource
+
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.extend(ClassMethods)
+    end
+
     attr_accessor :json_folder_path
 
     # add these as class methods
     module ClassMethods
-      def create(json_folder_path)
+      def create(json_folder_path: nil, params: nil)
         instance = self.new
         instance.json_folder_path = json_folder_path
+        instance.after_creation(params)
         return instance
+      end
+    end
+
+    # add this as instance methods
+    module InstanceMethods
+      def after_creation(params)
       end
     end
   end
