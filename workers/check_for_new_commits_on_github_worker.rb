1b5d7656aa603b848099a6c009e205a2d683b07c
Joshua Liebowitz <taquitos@google.com>
2018-02-15 17:21:31 -0800

    Update github commit worker to use serial queue - add all checking tasks to queue - don't add more tasks until the previous set finished - updated worker_base to do work, then call sleep instead of sleep first

--- workers/check_for_new_commits_on_github_worker.rb
+++ workers/check_for_new_commits_on_github_worker.rb
@@ -15,6 +15,8 @@ module FastlaneCI
     attr_accessor :provider_credential
     attr_accessor :project
     attr_accessor :user_config_service
+    attr_accessor :serial_task_queue
+    attr_accessor :current_tasks
 
     attr_writer :git_repo
 
@@ -26,16 +28,26 @@ module FastlaneCI
       self.provider_credential = provider_credential
       self.project = project
 
-      super() # This starts the work by calling `work`
-
-      time_nano = Time.now.nsec
       project_full_name = project.repo_config.git_url
 
       if project.repo_config.kind_of?(FastlaneCI::GitRepoConfig)
         project_full_name = project.repo_config.full_name unless project.repo_config.full_name.nil?
       end
 
-      self.thread_id = "GithubWorker:#{time_nano}: #{project_full_name}: #{provider_credential.ci_user.email}"
+      self.serial_task_queue = TaskQueue::TaskQueue.new(name: "#{project_full_name}:#{provider_credential.ci_user.email}")
+      self.current_tasks = []
+
+      super() # This starts the work by calling `work`
+    end
+
+    def thread_id
+      if @thread_id
+        return @thread_id
+      end
+
+      time_nano = Time.now.nsec
+      @thread_id = "GithubWorker:#{time_nano}: #{self.serial_task_queue.name}"
+      return @thread_id
     end
 
     # This is a separate method, so it's lazy loaded
@@ -48,57 +60,88 @@ module FastlaneCI
       )
     end
 
+    def wait_for_previous_tasks?
+      # see how many tasks have not completed
+      not_finished_tasks = self.current_tasks.reject(&:completed)
+
+      # if we have any tasks that are not complete, return true so that we don't enque more
+      return not_finished_tasks.length > 0
+    end
+
     def work
       if ENV["FASTLANE_CI_SUPER_VERBOSE"]
         logger.debug("Checking for new commits on GitHub")
       end
+
+      logger.debug("Checking if we should wait for any previous previous tasks to complete")
+      should_wait = self.wait_for_previous_tasks?
+      if should_wait
+        logger.debug("We still have test runner tasks to finish, not enqueuing any more")
+        return
+      end
+
+      logger.debug("No old test runner tasks, enqueuing new runner tasks")
       repo = self.git_repo
 
-      # is needed to see if there are new branches
-      # called async
+      # is needed to see if there are new branches (called async)
       repo.fetch
 
       # TODO: ensure BuildService subclasses are thread-safe
       build_service = FastlaneCI::Services.build_service
 
+      self.current_tasks = []
       repo.git_and_remote_branches_each do |git, branch|
-        logger.debug("FOUND WEIRD BRANCH") if branch.name.start_with?("HEAD ->")
+        if ENV["FASTLANE_CI_SUPER_VERBOSE"]
+          # Not sure why this matters
+          logger.debug("FOUND WEIRD BRANCH") if branch.name.start_with?("HEAD ->")
+        end
 
         next if branch.name.start_with?("HEAD ->") # not sure what this is for
-        # Check out the specific branch
-        # this will detach our current head
-        git.reset_hard # as there might be un-committed changes in there
+
+        # There might be un-committed changes in there, so ignore
+        git.reset_hard
+
+        # Check out the specific branch, this will detach our current head
         branch.checkout
+
         current_sha = repo.most_recent_commit.sha
 
         builds = build_service.list_builds(project: self.project)
         if builds.map(&:sha).include?(current_sha)
           next
         end
+
         if ENV["FASTLANE_CI_SUPER_VERBOSE"]
-          logger.debug("Detected new branch #{branch.name} with sha #{current_sha}")
+          logger.debug("Detected branch #{branch.name} with sha #{current_sha}")
         end
+
         credential = self.provider_credential
         current_project = self.project
-        thread = Thread.new do
+
+        # This never stops because with each commit, it creates a new commit and then we think it's new, LOL
+        check_for_commit_task = TaskQueue::Task.new(work_block: proc {
           FastlaneCI::TestRunnerService.new(
             project: current_project,
             sha: current_sha,
             provider_credential: credential
           ).run
-        end
+        })
+
+        # We need this so we can check later if we should skip creating new test runner tasks
+        self.current_tasks << check_for_commit_task
 
         project_full_name = current_project.repo_config.git_url
         if current_project.repo_config.kind_of?(FastlaneCI::GitRepoConfig)
           project_full_name = current_project.repo_config.full_name unless current_project.repo_config.full_name.nil?
         end
 
-        thread[:thread_id] = "GithubWorkerChild: #{project_full_name}: #{credential.ci_user.email}: #{current_sha[-6..-1]}"
+        logger.debug("Adding task for #{project_full_name}: #{credential.ci_user.email}: #{current_sha[-6..-1]}")
+        self.serial_task_queue.add_task_async(task: check_for_commit_task)
       end
     end
 
     def sleep_interval
-      5
+      10
     end
   end
 end
