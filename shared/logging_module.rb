5c6ef13ce0bfd4770f93b9c34e6373e535481621
Stefan Natchev <snatchev@google.com>
2018-02-18 12:32:03 -0500

    refactor logging to make idiomatic use of ruby log levels

--- shared/logging_module.rb
+++ shared/logging_module.rb
@@ -3,6 +3,29 @@ require "logger"
 
 module FastlaneCI
   # Logging mixin, can be used wherever
+  #
+  #
+  # The available log levels are:
+  #   * UNKNOWN - An unknown message that should always be logged.
+  #   * FATAL - An unhandleable error that results in a program crash.
+  #   * ERROR - A handleable error condition
+  #   * WARN - A warning
+  #   * INFO - Generic (useful) information about system operation
+  #   * DEBUG - Low-level information for developers
+  #
+  # from most-severe to least.
+  #
+  # The default log level is WARN, so anything more severe than a warning will be logged out.
+  #
+  # Setting the environment variable `DEBUG=1` will lower the log level to INFO.
+  # `logger.info` should be used to print out generic (and useful) information about
+  # system operation.
+  #
+  # Setting the environment variable `FASTLANE_CI_SUPER_VERBOSE=1` further lowers the log to DEBUG.
+  # This includes extra logging which includes thread ids, and other
+  # non-essential information that could be useful during debugging.
+  #
+  # Use `logger.debug` for exposing this extra information
   module Logging
     def logger
       @logger ||= Logger.new(STDOUT).tap do |logger|
@@ -14,6 +37,8 @@ module FastlaneCI
             class_name = self.name.split("::").last
           end
 
+          progname ||= class_name
+
           thread_id = ""
 
           if ENV["FASTLANE_CI_SUPER_VERBOSE"]
@@ -21,9 +46,14 @@ module FastlaneCI
             thread_id = " #{Thread.current[:thread_id]}" unless Thread.current[:thread_id].nil?
           end
 
-          "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}#{thread_id}] #{severity} #{class_name}:  #{msg}\n"
+          "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}#{thread_id}] #{severity} #{progname}:  #{msg}\n"
         end
+
+        logger.level = :warn
+        logger.level = :info if ENV['DEBUG']
+        logger.level = :debug if ENV["FASTLANE_CI_SUPER_VERBOSE"]
       end
+
       return @logger
     end
   end
