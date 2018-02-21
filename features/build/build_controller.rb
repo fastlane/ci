1e5f9a7ed1efda1f5b3ae2b04fa565b948842635
Felix Krause <krausefx@gmail.com>
2018-02-21 17:53:35 -0500

    Work on more sample code

--- features/build/build_controller.rb
+++ features/build/build_controller.rb
@@ -1,11 +1,18 @@
 require_relative "../../shared/authenticated_controller_base"
 require "pathname"
-require "sinatra-websocket"
 
 module FastlaneCI
   # Controller for a single project view. Responsible for updates, triggering builds, and displaying project info
   class BuildController < AuthenticatedControllerBase
     HOME = "/projects/*/builds"
+
+    # get "/projects*/builds/*/stream" do |project_id, build_id|
+    #   stream do |out|
+    #     out << "It's gonna be legen -\n"
+    #     sleep 2
+    #     out << "- dary!\n"
+    #   end
+    # end
 
     get "/projects/*/builds/*" do |project_id, build_id|
       project = self.user_project_with_id(project_id: project_id)
@@ -14,30 +21,10 @@ module FastlaneCI
       locals = {
         project: project,
         build: build,
+        build_output: build.full_log,
         title: "Project #{project.project_name}, Build #{build.sha}"
       }
       erb(:build, locals: locals, layout: FastlaneCI.default_layout)
-    end
-
-    get "/projects*/builds/*/stream" do |project_id, build_id|
-      if request.websocket?
-        request.websocket do |ws|
-          ws.onopen do
-            ws.send("Hello World!")
-            settings.sockets << ws
-          end
-          ws.onmessage do |msg|
-            EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-          end
-          ws.onclose do
-            warn("websocket closed")
-            settings.sockets.delete(ws)
-          end
-        end
-      else
-        require 'pry'; binding.pry
-        puts 'hi'
-      end
     end
   end
 end
