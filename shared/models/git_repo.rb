99fe676502f66f04fd7938d8e1b7b6062b51a655
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-02-22 13:20:46 -0500

    Disable Performance/RedundantBlockCall in rubocop settings.

--- shared/models/git_repo.rb
+++ shared/models/git_repo.rb
@@ -164,7 +164,7 @@ module FastlaneCI
         logger.debug("iterating through all remote branches of #{self.git_config.git_url}")
         branch_count = 0
         self.git.branches.remote.each do |branch|
-          yield(self.git, branch)
+          each_block.call(self.git, branch)
           branch_count += 1
         end
         logger.debug("done iterating through all #{branch_count} remote branches of #{self.git_config.git_url}")
