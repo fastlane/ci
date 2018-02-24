b4c92ffca336cae62ac4ef5fda45615df3c441f5
Jorge Revuelta <jrevuelta@usal.es>
2018-02-24 20:10:48 +0100

    Some refactoring done, tests remaining.

--- shared/json_convertible.rb
+++ shared/json_convertible.rb
@@ -45,51 +45,18 @@ module FastlaneCI
       def from_json!(json_object)
         instance = self.new
         json_object.each do |var, val|
-          if self.attribute_key_name_map.key(var)
-            var_name = self.attribute_key_name_map.key(var).to_sym
-          else
-            var_name = "@#{var}".to_sym
-          end
           # If we encounter with a value that is represented by an array, iterate over it.
           # TODO: Refactor this in a sepparate protected method to avoid code duplication. DRY.
           if val.kind_of?(Array)
             array_property = []
             val.each do |array_var, array_val|
-              this_instance = nil
-              if self.attribute_key_name_map.key(array_var)
-                array_var_name = self.attribute_key_name_map.key(array_var).to_sym
-              else
-                array_var_name = "@#{array_var}".to_sym
-              end
-              if self.json_to_attribute_name_proc_map.key?(array_var_name)
-                this_instance = self.json_to_attribute_name_proc_map[array_var_name].call(array_val)
-              else
-                this_instance = array_val
-              end
-              if self.attribute_to_type_map.key?(array_var_name)
-                if self.attribute_to_type_map[array_var_name].include?(FastlaneCI::JSONConvertible)
-                  # classes that include `JSONConvertible` take precedence over custom mapping.
-                  this_instance = self.attribute_to_type_map[array_var_name].from_json!(array_val)
-                end
-              else
-                this_instance = self.map_enumerable_type(enumerable_property_name: array_var_name, current_json_object: array_val)
-              end
+              _array_var_name, this_instance = self._from_json!(array_var, array_val, is_iterable: true)
               # What to do if we get `this_instance` nil at this point?
               array_property << this_instance
             end
             instance.instance_variable_set(var_name, array_property)
           else
-            if self.json_to_attribute_name_proc_map.key?(var_name)
-              var_value = self.json_to_attribute_name_proc_map[var_name].call(val)
-            else
-              var_value = val
-            end
-            if self.attribute_to_type_map.key?(var_name)
-              if self.attribute_to_type_map[var_name].include?(FastlaneCI::JSONConvertible)
-                # classes that include `JSONConvertible` take precedence over custom mapping.
-                var_value = self.attribute_to_type_map[var_name].from_json!(val)
-              end
-            end
+            var_name, var_value = self._from_json!(var, val)
             instance.instance_variable_set(var_name, var_value)
           end
         end
@@ -173,6 +140,30 @@ module FastlaneCI
       # TODO: Document, lazy.
       def self.map_enumerable_type(enumerable_property_name: nil, current_json_object: nil)
         return nil
+      end
+
+      protected
+
+      def _from_json!(var, val, is_iterable: false)
+        if self.attribute_key_name_map.key(var)
+          var_name = self.attribute_key_name_map.key(var).to_sym
+        else
+          var_name = "@#{var}".to_sym
+        end
+        if self.json_to_attribute_name_proc_map.key?(var_name)
+          var_value = self.json_to_attribute_name_proc_map[var_name].call(val)
+        else
+          var_value = val
+        end
+        if self.attribute_to_type_map.key?(var_name)
+          if self.attribute_to_type_map[var_name].include?(FastlaneCI::JSONConvertible)
+            # classes that include `JSONConvertible` take precedence over custom mapping.
+            var_value = self.attribute_to_type_map[var_name].from_json!(val)
+          end
+        elsif is_iterable
+          var_value = self.map_enumerable_type(enumerable_property_name: var_name, current_json_object: val)
+        end
+        return var_name, var_value
       end
     end
   end
