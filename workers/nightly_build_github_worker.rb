09fcb825e52634530484bf899058ab9afb209f00
nakhbari <nakhbari@users.noreply.github.com>
2018-03-02 11:35:41 -0800

    Add Nightly GitHub worker, but not using it yet (#199)
    Add Nightly GitHub worker, but not using it yet

---
+++ workers/nightly_build_github_worker.rb
@@ -0,0 +1,32 @@
+require_relative "github_worker_base"
+require_relative "../shared/models/job_trigger"
+require_relative "../shared/logging_module"
+
+module FastlaneCI
+  # Responsible for starting off builds nightly
+  class NightlyBuildGithubWorker < GitHubWorkerBase
+    include FastlaneCI::Logging
+    NIGHTLY_CRON_TIME = "0 0 * * *"
+
+    attr_accessor :scheduler
+    attr_accessor :trigger_type
+
+    def initialize(provider_credential: nil, project: nil)
+      self.trigger_type = FastlaneCI::JobTrigger::TRIGGER_TYPE[:nightly]
+      self.scheduler = WorkerScheduler.new(cron_schedule: NIGHTLY_CRON_TIME)
+
+      super(provider_credential: provider_credential, project: project) # This starts the work by calling `work`
+    end
+
+    def work
+      logger.debug("Running nightly builds on GitHub")
+
+      self.target_branches do |git, branch|
+        current_sha = repo.most_recent_commit.sha
+        logger.debug("Running Nightly build on branch #{branch.name} with sha #{current_sha}")
+
+        self.create_and_queue_build_task
+      end
+    end
+  end
+end
