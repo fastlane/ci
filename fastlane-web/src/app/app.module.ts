de58ec3340680397deb8481e0558855baccca253
Nima Akhbari <nnakhbari@gmail.com>
2018-03-14 20:52:30 -0700

    Add basic routing

--- fastlane-web/src/app/app.module.ts
+++ fastlane-web/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
