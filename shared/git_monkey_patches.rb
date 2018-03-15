abb17d9c62e5cd397bf7689ff9d6f3fde74d53d3
Joshua Liebowitz <taquitos@google.com>
2018-03-15 09:03:31 -0700

    Add attribution and license for source

--- shared/git_monkey_patches.rb
+++ shared/git_monkey_patches.rb
@@ -1,4 +1,19 @@
 module Git
+  # Lib class from https://github.com/voxpupuli/modulesync/pull/144/files
+  #
+  # Copyright [2018] [Dave Wongillies https://github.com/davewongillies]
+
+  # Licensed under the Apache License, Version 2.0 (the "License");
+  # you may not use this file except in compliance with the License.
+  # You may obtain a copy of the License at
+
+  #     http://www.apache.org/licenses/LICENSE-2.0
+
+  # Unless required by applicable law or agreed to in writing, software
+  # distributed under the License is distributed on an "AS IS" BASIS,
+  # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  # See the License for the specific language governing permissions and
+  # limitations under the License.
    class Lib
     # Monkey patch ls_files until https://github.com/ruby-git/ruby-git/pull/320 is resolved
     def ls_files(location=nil)
