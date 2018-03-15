13318a72e19b1cd8004d71264ad0265f7da764eb
Nima Akhbari <nnakhbari@gmail.com>
2018-03-14 20:52:30 -0700

    Add basic routing

---
+++ fastlane-web/src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule]})
+
+export class AppRoutingModule {
+}
