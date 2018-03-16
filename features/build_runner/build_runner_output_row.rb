77a785e2c070355f1986f67cfee263bd6596b25d
Felix Krause <krausefx@gmail.com>
2018-03-16 11:49:23 -0400

    Optimize build fail types, feedback via @taquitos

--- features/build_runner/build_runner_output_row.rb
+++ features/build_runner/build_runner_output_row.rb
@@ -3,6 +3,8 @@ module FastlaneCI
   # meaning it's a single message that we want to show to the user
   # and store as a build artifact as part of the build log
   class BuildRunnerOutputRow
+    BUILD_FAIL_TYPES = [:user_error, :build_error, :crash, :shell_error, :build_failure, :test_failure, :abort].to_set
+
     # The type of message (e.g. `:message`, `:error`, `:important`)
     attr_accessor :type
 
@@ -23,8 +25,11 @@ module FastlaneCI
 
     # Did this particular message fail the build? (e.g. `user_error` or `build_error`)
     def did_fail_build?
-      return true if [:user_error, :build_error, :crash, :shell_error, :build_failure, :test_failure, :abort].include?(self.type)
-      return false
+      # The first time this method is called, we check if this row failed the build
+      if @_did_fail_build.nil?
+        @_did_fail_build = BUILD_FAIL_TYPES.include?(self.type)
+      end
+      return @_did_fail_build
     end
 
     def to_json
