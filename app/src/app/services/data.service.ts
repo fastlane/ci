44081afafc149946d4bb8011c59753afa18c4d07
nakhbari <nakhbari@users.noreply.github.com>
2018-03-22 10:54:39 -0700

    Added JSON endpoint for projects and data server in the app (#342)
    * Added JSON endpoint for projects and data server in the app
    
    * json -> JSON

---
+++ app/src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {map} from 'rxjs/operators';
+
+import {Project, ProjectResponse} from '../models/project';
+
+// Data server is currently locally hosted.
+const HOSTNAME = '';
+
+@Injectable()
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<Project[]> {
+    const url = `${HOSTNAME}/projects`;
+    return this.http.get<ProjectResponse[]>(url).pipe(
+        map((projects) => projects.map((project) => new Project(project))));
+  }
+}
---
+++ features-json/project_json_controller.rb
@@ -0,0 +1,16 @@
+require_relative "../shared/authenticated_controller_base"
+
+module FastlaneCI
+  # Controller for providing all data relating to projects
+  class ProjectJSONController < AuthenticatedControllerBase
+    HOME = "/projects"
+
+    get "#{HOME}" do
+      current_provider_credential = self.check_and_get_provider_credential
+      current_user_config_service = self.current_user_config_service
+      all_projects = current_user_config_service.projects(provider_credential: current_provider_credential)
+
+      return all_projects.to_json
+    end
+  end
+end
