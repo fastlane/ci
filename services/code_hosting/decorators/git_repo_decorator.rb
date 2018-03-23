b6bef0fa84472c5af5d241ffbcc71613e51790d1
Jorge Revuelta <jrevuelta@usal.es>
2018-03-23 13:03:45 +0100

    Defaults to configuration_git_repo

--- services/code_hosting/decorators/git_repo_decorator.rb
+++ services/code_hosting/decorators/git_repo_decorator.rb
@@ -1,3 +1,5 @@
+require_relative "../../services"
+
 module FastlaneCI
   # This module acts as a decorator to be added to any method that potentially will make changes
   # into the ci-config repository directory.
@@ -28,7 +30,7 @@ module FastlaneCI
       end
     end
 
-    def pull_before(func_name, git_repo: nil)
+    def pull_before(func_name, git_repo: Services.configuration_git_repo)
       new_name_for_old_function = "#{func_name}_old".to_sym
       alias_method(new_name_for_old_function, func_name)
       define_method(func_name) do |*args|
@@ -46,7 +48,7 @@ module FastlaneCI
       end
     end
 
-    def commit_after(func_name, git_repo: nil)
+    def commit_after(func_name, git_repo: Services.configuration_git_repo)
       new_name_for_old_function = "#{func_name}_old".to_sym
       alias_method(new_name_for_old_function, func_name)
       define_method(func_name) do |*args|
