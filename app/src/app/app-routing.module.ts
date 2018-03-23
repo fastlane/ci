b8cdb6c400c597c8958d1e3eff753be8863eacec
nakhbari <nakhbari@users.noreply.github.com>
2018-03-23 13:18:38 -0700

    Added Overview page with initial Grid stylings (#366)
    * Added JSON endpoint for projects and data server in the app
    
    * json -> JSON
    
    * Added Welcome page text with initial grid styling
    
    * Removed merge conflict
    
    * Remove single grid columns
    
    * Remove MatGridListModule

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {OverviewComponent} from './overview/overview.component';
 
-const routes: Routes = [{path: '', component: DashboardComponent}];
+const routes: Routes = [
+  {path: '', redirectTo: '/overview', pathMatch: 'full'},
+  {path: 'overview', component: OverviewComponent}
+];
 
 @NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule]})
 
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,15 +1,17 @@
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './/app-routing.module';
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-
+import {OverviewComponent} from './overview/overview.component';
+import {DataService} from './services/data.service';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, DashboardComponent, OverviewComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
