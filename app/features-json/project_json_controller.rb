f122d6bcbd3bd0c68aa5580b6269f57a133ac26a
Nima Akhbari <nnakhbari@gmail.com>
2018-03-27 13:47:02 -0700

    Add new proto file to control what we expose via json api

--- app/features-json/project_json_controller.rb
+++ app/features-json/project_json_controller.rb
@@ -1,4 +1,5 @@
 require_relative "../shared/authenticated_controller_base"
+require_relative "./proto/project_summary"
 
 module FastlaneCI
   # Controller for providing all data relating to projects
@@ -9,8 +10,11 @@ module FastlaneCI
       current_provider_credential = self.check_and_get_provider_credential
       current_user_config_service = self.current_user_config_service
       all_projects = current_user_config_service.projects(provider_credential: current_provider_credential)
+      all_projects_proto = all_projects.map do |project|
+        FastlaneCI::Proto::ProjectSummary.new(project_name: project.project_name, id: project.project_id)
+      end
 
-      return all_projects.to_json
+      return all_projects_proto.to_json
     end
   end
 end
