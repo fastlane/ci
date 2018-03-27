10a531bfcfd1c5d9d153505252b3df1a5116098e
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2018-03-27 11:32:10 +0300

    CI keys config path fix

---
+++ spec/services/environment_variable_service_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+require "app/services/environment_variable_service"
+
+describe FastlaneCI::EnvironmentVariableService do
+
+  describe "#keys_file_path" do
+    before(:each) do
+      allow(Dir).to receive(:home).and_return("/path/to/home")
+    end
+    it "has correct file path" do
+      expect(FastlaneCI::EnvironmentVariableService.new.keys_file_path).to eq("/path/to/home/.fastlane/ci/.keys")
+    end
+  end
+
+  describe "#keys_file_path_relative_to_home" do
+    it "has correct file path" do
+      expect(FastlaneCI::EnvironmentVariableService.new.keys_file_path_relative_to_home).to eq("~/.fastlane/ci/.keys")
+    end
+  end
+end
