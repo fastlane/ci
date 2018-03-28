d4d4f9b2d220a7f92f36b5298a04454882d53167
Jorge Revuelta <jrevuelta@usal.es>
2018-03-29 01:05:00 +0200

    Added tests

--- spec/stub_helpers.rb
+++ spec/stub_helpers.rb
@@ -37,6 +37,9 @@ module StubHelpers
     allow_any_instance_of(FastlaneCI::GitRepo).to receive(:git_config).and_return(fake_git_config)
     allow_any_instance_of(FastlaneCI::GitRepo).to receive(:repo_auth).and_return(fake_repo_auth)
     allow_any_instance_of(FastlaneCI::GitRepo).to receive(:clone)
+    FastlaneCI::GitRepo.any_instance.stub(:pull)
+    FastlaneCI::GitRepo.any_instance.stub(:push)
+    FastlaneCI::GitRepo.any_instance.stub(:commit_changes!)
   end
 
   def stub_services
