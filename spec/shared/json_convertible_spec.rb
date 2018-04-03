55d70703b4325da429699e3e85f2af9f0ffc37ab
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-04-03 02:42:21 -0400

    Exclude `specs` for `120` line character limit.

--- spec/shared/json_convertible_spec.rb
+++ spec/shared/json_convertible_spec.rb
@@ -101,40 +101,22 @@ module FastlaneCI
     end
 
     it "Allows to object dictionary with custom property mapping" do
-      allow(
-        MockJSONConvertible
-      ).to receive(:attribute_key_name_map).and_return({ :@one_attribute => "json_one_attribute" })
+      allow(MockJSONConvertible).to receive(:attribute_key_name_map).and_return({ :@one_attribute => "json_one_attribute" })
       json_from_mock_object = mock_object.to_object_dictionary
       expect(json_from_mock_object).to eql({ "json_one_attribute" => "Hello", "other_attribute" => Time.at(0) })
     end
 
     it "Allows to perform custom mappings of the values for a given attribute when converting to JSON" do
-      allow(
-        MockJSONConvertible
-      ).to receive(:attribute_name_to_json_proc_map).and_return(
-        { :@other_attribute => proc { |timestamp| timestamp.strftime("%G-W%V-%uT%R%:z") } }
-      )
+      allow(MockJSONConvertible).to receive(:attribute_name_to_json_proc_map).and_return({ :@other_attribute => proc { |timestamp| timestamp.strftime("%G-W%V-%uT%R%:z") } })
       json_from_mock_object = mock_object.to_object_dictionary
-      expect(
-        json_from_mock_object
-      ).to eql({ "one_attribute" => "Hello", "other_attribute" => Time.at(0).strftime("%G-W%V-%uT%R%:z") })
+      expect(json_from_mock_object).to eql({ "one_attribute" => "Hello", "other_attribute" => Time.at(0).strftime("%G-W%V-%uT%R%:z") })
     end
 
     it "Allows to perform custom property mapping and value mapping at the same time" do
-      allow(
-        MockJSONConvertible
-      ).to receive(:attribute_key_name_map).and_return(
-        { :@one_attribute => "json_one_attribute", :@other_attribute => "json_other_attribute" }
-      )
-      allow(
-        MockJSONConvertible
-      ).to receive(:attribute_name_to_json_proc_map).and_return(
-        { :@other_attribute => proc { |timestamp| timestamp.strftime("%G-W%V-%uT%R%:z") } }
-      )
+      allow(MockJSONConvertible).to receive(:attribute_key_name_map).and_return({ :@one_attribute => "json_one_attribute", :@other_attribute => "json_other_attribute" })
+      allow(MockJSONConvertible).to receive(:attribute_name_to_json_proc_map).and_return({ :@other_attribute => proc { |timestamp| timestamp.strftime("%G-W%V-%uT%R%:z") } })
       json_from_mock_object = mock_object.to_object_dictionary
-      expect(
-        json_from_mock_object
-      ).to eql({ "json_one_attribute" => "Hello", "json_other_attribute" => Time.at(0).strftime("%G-W%V-%uT%R%:z") })
+      expect(json_from_mock_object).to eql({ "json_one_attribute" => "Hello", "json_other_attribute" => Time.at(0).strftime("%G-W%V-%uT%R%:z") })
     end
 
     it "Allows to decode from a dictionary object" do
@@ -146,45 +128,29 @@ module FastlaneCI
     end
 
     it "Allows to decode from a dictionary object using custom property mapping" do
-      allow(
-        MockJSONConvertible
-      ).to receive(:attribute_key_name_map).and_return({ :@one_attribute => "json_one_attribute" })
+      allow(MockJSONConvertible).to receive(:attribute_key_name_map).and_return({ :@one_attribute => "json_one_attribute" })
       object = MockJSONConvertible.from_json!({ "json_one_attribute" => "Hello", "other_attribute" => Time.at(0) })
       expect(object.one_attribute).to eql("Hello")
       expect(object.other_attribute).to eql(Time.at(0))
     end
 
     it "Allows to decode from a dictionary object using custom value mapping" do
-      allow(
-        MockJSONConvertible
-      ).to receive(:json_to_attribute_name_proc_map).and_return(
-        { :@other_attribute => proc { |timestamp| Time.at(timestamp) } }
-      )
+      allow(MockJSONConvertible).to receive(:json_to_attribute_name_proc_map).and_return({ :@other_attribute => proc { |timestamp| Time.at(timestamp) } })
       object = MockJSONConvertible.from_json!({ "one_attribute" => "Hello", "other_attribute" => Time.at(0) })
       expect(object.one_attribute).to eql("Hello")
       expect(object.other_attribute).to eql(Time.at(0))
     end
 
     it "Allows to decode from a dictionary object using both custom value mapping and custom porperty mapping" do
-      allow(
-        MockJSONConvertible
-      ).to receive(:attribute_key_name_map).and_return(
-        { :@one_attribute => "json_one_attribute", :@other_attribute => "json_other_attribute" }
-      )
-      allow(
-        MockJSONConvertible
-      ).to receive(:json_to_attribute_name_proc_map).and_return(
-        { :@other_attribute => proc { |timestamp| Time.at(timestamp) } }
-      )
+      allow(MockJSONConvertible).to receive(:attribute_key_name_map).and_return({ :@one_attribute => "json_one_attribute", :@other_attribute => "json_other_attribute" })
+      allow(MockJSONConvertible).to receive(:json_to_attribute_name_proc_map).and_return({ :@other_attribute => proc { |timestamp| Time.at(timestamp) } })
       object = MockJSONConvertible.from_json!({ "json_one_attribute" => "Hello", "json_other_attribute" => Time.at(0) })
       expect(object.one_attribute).to eql("Hello")
       expect(object.other_attribute).to eql(Time.at(0))
     end
 
     it "Allows to decode from a dictionary object when it has custom attributes" do
-      allow(
-        MockParentJSONConvertible
-      ).to receive(:attribute_to_type_map).and_return({ :@mock_json_attribute => MockJSONConvertible })
+      allow(MockParentJSONConvertible).to receive(:attribute_to_type_map).and_return({ :@mock_json_attribute => MockJSONConvertible })
       dictionary_object = { "mock_json_attribute" => { "one_attribute" => "Hello", "other_attribute" => Time.at(0) } }
       object = MockParentJSONConvertible.from_json!(dictionary_object)
       expect(object.mock_json_attribute.one_attribute).to eql("Hello")
@@ -192,18 +158,10 @@ module FastlaneCI
     end
 
     it "The mapping of a class that includes `JSONConvertible` takes precedence from a custom map of a parent class" do
-      allow(
-        MockParentJSONConvertible
-      ).to receive(:attribute_to_type_map).and_return({ :@mock_json_attribute => MockJSONConvertible })
-      allow(
-        MockJSONConvertible
-      ).to receive(:json_to_attribute_name_proc_map).and_return(
-        {
-          :@mock_json_attribute => proc { |_|
-            MockJSONConvertible.new(one_attribute: "World", other_attribute: Time.at(10))
-          }
-        }
-      )
+      allow(MockParentJSONConvertible).to receive(:attribute_to_type_map).and_return({ :@mock_json_attribute => MockJSONConvertible })
+      allow(MockJSONConvertible).to receive(:json_to_attribute_name_proc_map).and_return({ :@mock_json_attribute => proc { |_|
+        MockJSONConvertible.new(one_attribute: "World", other_attribute: Time.at(10))
+      } })
       dictionary_object = { "mock_json_attribute" => { "one_attribute" => "Hello", "other_attribute" => Time.at(0) } }
       object = MockParentJSONConvertible.from_json!(dictionary_object)
       expect(object.mock_json_attribute.one_attribute).to eql("Hello")
@@ -211,16 +169,8 @@ module FastlaneCI
     end
 
     it "Allows to decode from a dictionary object when it has an array typed attribute" do
-      allow(
-        MockArrayJSONConvertible
-      ).to receive(:attribute_to_type_map).and_return({ :@mock_json_array_attribute => MockJSONConvertible })
-      dictionary_object = {
-        "mock_json_array_attribute" => [
-          { "one_attribute" => "Hello", "other_attribute" => Time.at(0) },
-          { "one_attribute" => "World", "other_attribute" => Time.at(10) }
-        ]
-      }
-
+      allow(MockArrayJSONConvertible).to receive(:attribute_to_type_map).and_return({ :@mock_json_array_attribute => MockJSONConvertible })
+      dictionary_object = { "mock_json_array_attribute" => [{ "one_attribute" => "Hello", "other_attribute" => Time.at(0) }, { "one_attribute" => "World", "other_attribute" => Time.at(10) }] }
       object = MockArrayJSONConvertible.from_json!(dictionary_object)
       expect(object.mock_json_array_attribute.kind_of?(Array)).to eql(true)
       expect(object.mock_json_array_attribute.length).to eql(2)
@@ -276,25 +226,13 @@ module FastlaneCI
     end
 
     it "Allows to decode objects with nested array properties" do
-      mock_object = MockAttributeArrayJSONConvertible.new(
-        one_attribute: "World", other_attribute: Time.at(10),
-        array_attribute: [MockJSONConvertible.new(one_attribute: "Inner World", other_attribute: Time.at(100))]
-      )
+      mock_object = MockAttributeArrayJSONConvertible.new(one_attribute: "World", other_attribute: Time.at(10), array_attribute: [
+                                                            MockJSONConvertible.new(one_attribute: "Inner World", other_attribute: Time.at(100))
+                                                          ])
       dictionary_object = mock_object.to_object_dictionary
-      expect(
-        dictionary_object
-      ).to eql(
-        {
-          "one_attribute" => "World",
-          "other_attribute" => Time.at(10),
-          "array_attribute" => [
-            {
-              "one_attribute" => "Inner World",
-              "other_attribute" => Time.at(100)
-            }
-          ]
-        }
-      )
+      expect(dictionary_object).to eql({ "one_attribute" => "World", "other_attribute" => Time.at(10), "array_attribute" => [
+                                         { "one_attribute" => "Inner World", "other_attribute" => Time.at(100) }
+                                       ] })
     end
   end
 end
