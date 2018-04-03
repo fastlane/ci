37dcb1f7652d8ad647e7cf551db26c59685d1256
Jorge Revuelta <jrevuelta@usal.es>
2018-04-03 12:09:18 +0200

    Fix Rubocop

--- app/features/login/login_controller.rb
+++ app/features/login/login_controller.rb
@@ -36,7 +36,6 @@ module FastlaneCI
 
     # Login with fastlane.ci credentials
     get "#{HOME}/ci_login" do
-
       client = Octokit::Client.new(access_token: FastlaneCI.env.clone_user_api_token)
 
       unless client.nil?
