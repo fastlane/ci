1276f026d6965caeee22536b208b7b04a6a92dbb
Nima Akhbari <nnakhbari@gmail.com>
2018-04-03 15:02:29 -0700

    Moved to using a list with some placeholders

--- web/app/app.module.ts
+++ web/app/app.module.ts
@@ -2,6 +2,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './/app-routing.module';
@@ -18,6 +19,7 @@ import {DataService} from './services/data.service';
     AppRoutingModule,
     /** Angular Material Imports */
     MatCardModule,
+    MatListModule,
     MatIconModule,
   ],
   providers: [DataService],
