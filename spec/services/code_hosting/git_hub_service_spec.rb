46f702b7b60f5e3f0317ce11bcb3005cbfdc9b19
Andrew McBurney <andrewrobertmcburney@gmail.com>
2018-04-08 11:29:01 -0400

    Fixed specs for `git_hub_service_spec.rb`. (#2)

--- spec/services/code_hosting/git_hub_service_spec.rb
+++ spec/services/code_hosting/git_hub_service_spec.rb
@@ -3,17 +3,28 @@ require "app/services/code_hosting/git_hub_service"
 
 describe FastlaneCI::GitHubService do
   describe ".token_scope_validation_error" do
-    before(:each) do
-      client_instance = an_instance_of(Octokit::Client)
-      allow(Octokit::Client).to receive(:new).and_return(client_instance)
-      allow(client_instance).to receive(:scopes).with("invalid_token").and_return(["_unrecognized_scope"])
-      allow(client_instance).to receive(:scopes).with("valid_token").and_return(["repo"])
+    context "invalid_token" do
+      before(:each) do
+        client_instance = an_instance_of(Octokit::Client)
+        allow(Octokit::Client).to receive(:new).with(access_token: "invalid_token").and_return(client_instance)
+        allow(client_instance).to receive(:scopes).and_return(["_unrecognized_scope"])
+      end
+
+      it "returns validation error properties if token invalid" do
+        expect(FastlaneCI::GitHubService.token_scope_validation_error("invalid_token")).to eq([["_unrecognized_scope"], "repo"])
+      end
     end
-    it "returns validation error properties if token invalid" do
-      expect(FastlaneCI::GitHubService.token_scope_validation_error("invalid_token")).to eq([["_unrecognized_scope"], "repo"])
-    end
-    it "returns nil if token valid" do
-      expect(FastlaneCI::GitHubService.token_scope_validation_error("valid_token")).to be_nil
+
+    context "valid token" do
+      before(:each) do
+        client_instance = an_instance_of(Octokit::Client)
+        allow(Octokit::Client).to receive(:new).with(access_token: "valid_token").and_return(client_instance)
+        allow(client_instance).to receive(:scopes).and_return(["repo"])
+      end
+
+      it "returns nil if token valid" do
+        expect(FastlaneCI::GitHubService.token_scope_validation_error("valid_token")).to be_nil
+      end
     end
   end
 end
