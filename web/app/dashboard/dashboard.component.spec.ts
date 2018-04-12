6bc9b9cd9a462c6bf4be4975cf08c75b8a5c8970
Nima Akhbari <nnakhbari@gmail.com>
2018-04-12 09:24:55 -0700

    Add project list to the Dashboard

--- web/app/dashboard/dashboard.component.spec.ts
+++ web/app/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import 'rxjs/add/observable/of';
 
-import { DashboardComponent } from './dashboard.component';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {Observable} from 'rxjs/Observable';
+
+import {BuildStatus} from '../common/constants';
+import {asyncData} from '../common/testing/async_observable_helpers';
+import {DataService} from '../services/data.service';
+
+import {DashboardComponent} from './dashboard.component';
+import {mockProjectSummaryList} from './test_helpers/mock_project_summary';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let dataService: jasmine.SpyObj<Partial<DataService>>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
+      beforeEach(async(() => {
+        dataService = {getProjects: jasmine.createSpy()};
+        TestBed
+            .configureTestingModule({
+              imports: [MatIconModule, MatCardModule, MatTableModule],
+              declarations: [
+                DashboardComponent,
+              ],
+              providers: [{provide: DataService, useValue: dataService}],
+            })
+            .compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DashboardComponent);
+        component = fixture.componentInstance;
+      }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should load project summaries', fakeAsync(() => {
+       dataService.getProjects.and.returnValue(
+           Observable.of(mockProjectSummaryList))
+
+       expect(component.isLoading).toBe(true);
+
+       fixture.detectChanges();  // onInit()
+       tick();                   // Observable is resolved
+
+       expect(component.isLoading).toBe(false);
+       expect(component.projects.length).toBe(3);
+       expect(component.projects[0].id).toBe('1');
+       expect(component.projects[0].name).toBe('the coolest project');
+       expect(component.projects[1].latestStatus).toBe(BuildStatus.SUCCESS);
+       expect(component.projects[2].statusIcon).toBe('error');
+     }));
 });
