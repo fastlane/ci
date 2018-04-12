b53ff4fe29c8d0d1e7a81fb42afd2236845f105a
Nima Akhbari <nnakhbari@gmail.com>
2018-04-12 11:32:02 -0700

    Fix tests

--- web/app/dashboard/dashboard.component.spec.ts
+++ web/app/dashboard/dashboard.component.spec.ts
@@ -5,9 +5,11 @@ import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
 import {Observable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
 
 import {BuildStatus} from '../common/constants';
 import {asyncData} from '../common/testing/async_observable_helpers';
+import {ProjectSummary} from '../models/project_summary';
 import {DataService} from '../services/data.service';
 
 import {DashboardComponent} from './dashboard.component';
@@ -16,38 +18,39 @@ import {mockProjectSummaryList} from './test_helpers/mock_project_summary';
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-  let dataService: jasmine.SpyObj<Partial<DataService>>
+  let dataService: jasmine.SpyObj<Partial<DataService>>;
 
-      beforeEach(async(() => {
-        dataService = {getProjects: jasmine.createSpy()};
-        TestBed
-            .configureTestingModule({
-              imports: [MatIconModule, MatCardModule, MatTableModule],
-              declarations: [
-                DashboardComponent,
-              ],
-              providers: [{provide: DataService, useValue: dataService}],
-            })
-            .compileComponents();
+  beforeEach(() => {
+    dataService = {getProjects: jasmine.createSpy()};
 
-        fixture = TestBed.createComponent(DashboardComponent);
-        component = fixture.componentInstance;
-      }));
+    TestBed
+        .configureTestingModule({
+          imports: [MatIconModule, MatCardModule, MatTableModule],
+          declarations: [
+            DashboardComponent,
+          ],
+          providers: [{provide: DataService, useValue: dataService}],
+        })
+        .compileComponents();
 
-  it('should load project summaries', fakeAsync(() => {
-       dataService.getProjects.and.returnValue(
-           Observable.of(mockProjectSummaryList))
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+  });
 
-       expect(component.isLoading).toBe(true);
+  it('should load project summaries', () => {
+    const subject = new Subject<ProjectSummary[]>();
+    dataService.getProjects.and.returnValue(subject.asObservable())
 
-       fixture.detectChanges();  // onInit()
-       tick();                   // Observable is resolved
+    expect(component.isLoading).toBe(true);
 
-       expect(component.isLoading).toBe(false);
-       expect(component.projects.length).toBe(3);
-       expect(component.projects[0].id).toBe('1');
-       expect(component.projects[0].name).toBe('the coolest project');
-       expect(component.projects[1].latestStatus).toBe(BuildStatus.SUCCESS);
-       expect(component.projects[2].statusIcon).toBe('error');
-     }));
+    fixture.detectChanges();               // onInit()
+    subject.next(mockProjectSummaryList);  // Resolve observable
+
+    expect(component.isLoading).toBe(false);
+    expect(component.projects.length).toBe(3);
+    expect(component.projects[0].id).toBe('1');
+    expect(component.projects[0].name).toBe('the coolest project');
+    expect(component.projects[1].latestStatus).toBe(BuildStatus.SUCCESS);
+    expect(component.projects[2].statusIcon).toBe('error');
+  });
 });
