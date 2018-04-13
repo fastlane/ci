a919de2998b6e5694056997cb939726e0b7c7087
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2018-04-13 10:20:16 +0200

    Search Fastfile recursively on local repo

--- app/shared/fastfile_finder.rb
+++ app/shared/fastfile_finder.rb
@@ -14,25 +14,25 @@ module FastlaneCI
 
     # if you're using this with the fastfile parser, you need to use `relative: false`
     def self.search_path(path:, relative_path: false)
-      # First assume the fastlane directory and its file is in the root of the project
-      fastfiles = Dir.glob(File.join(path, "fastlane/Fastfile"), File::FNM_CASEFOLD)
-      # If not, it might be in a subfolder
-      fastfiles = Dir.glob(File.join(path, "**/fastlane/Fastfile"), File::FNM_CASEFOLD) if fastfiles.count == 0
+      # fetch recursively for a Fastfile
+      fastfiles = Dir.glob(File.join(path, "**/Fastfile"), File::FNM_CASEFOLD)
+
+      if fastfiles.count == 0
+        directory_contents = Dir[File.expand_path(path)].join("\n")
+        logger.error("No Fastfile found at #{path}/fastlane/Fastfile, or any descendants")
+        logger.error("Directory contents: #{directory_contents}")
+        return nil
+      end
 
       if fastfiles.count > 1
         logger.error("Ugh, multiple Fastfiles found, we're gonna have to build a selection in the future")
         # for now, just take the first one
       end
 
-      if fastfiles.count == 0
-        directory_contents = Dir[File.expand_path(path)].join("\n")
-        logger.error("No Fastfile found at #{path}/fastlane/Fastfile, or any descendants")
-        logger.error("Directory contents: #{directory_contents}")
-      else
-        fastfile_path = fastfiles.first
-        if relative_path
-          fastfile_path = Pathname.new(fastfile_path).relative_path_from(Pathname.new(path))
-        end
+      # return the Fastfile at path/fastlane/Fastfile if present, otherwise the first one found
+      fastfile_path = fastfiles.find { |current_path| current_path == "#{path}/fastlane/Fastfile" } || fastfiles.first
+      if relative_path
+        fastfile_path = Pathname.new(fastfile_path).relative_path_from(Pathname.new(path))
       end
       return fastfile_path
     end
