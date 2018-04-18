7ec58f02db3bdb379409c5af2c512eef3d12515f
Nima Akhbari <nnakhbari@gmail.com>
2018-04-18 15:36:35 -0700

    Add Shared Material module, so we only have to import things one time

--- web/app/app.module.ts
+++ web/app/app.module.ts
@@ -1,10 +1,5 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {BrowserModule} from '@angular/platform-browser';
 import {MomentModule} from 'ngx-moment';
 
@@ -13,6 +8,7 @@ import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {ProjectComponent} from './project/project.component';
 import {DataService} from './services/data.service';
+import {SharedMaterialModule} from './shared_material.module';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, ProjectComponent],
@@ -21,11 +17,7 @@ import {DataService} from './services/data.service';
     HttpClientModule,
     AppRoutingModule,
     /** Angular Material Imports */
-    MatCardModule,
-    MatTableModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
+    SharedMaterialModule,
     /** Third-Party Module Imports */
     MomentModule,
   ],
