1a44ea51e070d03c4073b61e81765ae6c4d37fab
Nima Akhbari <nnakhbari@gmail.com>
2018-04-20 14:58:51 -0700

    Added repo name to project table

--- web/app/models/project_summary.ts
+++ web/app/models/project_summary.ts
@@ -4,6 +4,7 @@ export interface ProjectSummaryResponse {
   id: string;
   name: string;
   lane: string;
+  repo_name: string;
   latest_status?: FastlaneStatus;
   latest_timestamp?: string;
 }
@@ -12,6 +13,7 @@ export class ProjectSummary {
   readonly name: string;
   readonly id: string;
   readonly lane: string;
+  readonly repoName: string;
   readonly latestStatus?: BuildStatus;
   readonly latestDate?: Date;
   readonly statusIcon: string;
@@ -20,8 +22,13 @@ export class ProjectSummary {
     this.name = projectSummary.name;
     this.id = projectSummary.id;
     this.lane = projectSummary.lane;
-    this.latestStatus = projectSummary.latest_status ? fastlaneStatusToEnum(projectSummary.latest_status) : undefined;
+    this.repoName = projectSummary.repo_name;
+    this.latestStatus = projectSummary.latest_status ?
+        fastlaneStatusToEnum(projectSummary.latest_status) :
+        undefined;
     this.statusIcon = buildStatusToIcon(this.latestStatus);
-    this.latestDate = projectSummary.latest_timestamp ? new Date(projectSummary.latest_timestamp) : undefined;
+    this.latestDate = projectSummary.latest_timestamp ?
+        new Date(projectSummary.latest_timestamp) :
+        undefined;
   }
 }
