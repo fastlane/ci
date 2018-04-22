1fcbb8e5a670644c66db0a77406747b330de0769
Jorge Revuelta <jrevuelta@usal.es>
2018-04-22 20:40:49 +0200

    Test is love, test is life

--- web/app/common/test_helpers/mock_repository_data.ts
+++ web/app/common/test_helpers/mock_repository_data.ts
@@ -1,7 +1,35 @@
-import {RepositoryResponse} from '../../models/repository';
+import {Repository, RepositoryResponse} from '../../models/repository';
 
 export const mockRepositoryResponse: RepositoryResponse = {
   full_name: 'fastlane/ci',
   api_path: 'https://api.github.com/fastlane/ci',
   url: 'https://github.com/fastlane/ci'
 };
+
+export const mockRepositoryListResponse: RepositoryResponse[] = [
+  {
+    full_name: 'fastlane/ci',
+    api_path: 'https://api.github.com/fastlane/ci',
+    url: 'https://github.com/fastlane/ci'
+  },
+  {
+    full_name: 'fastlane/fastlane',
+    api_path: 'https://api.github.com/fastlane/fastlane',
+    url: 'https://github.com/fastlane/fastlane'
+  },
+  {
+    full_name: 'fastlane/onboarding',
+    api_path: 'https://api.github.com/fastlane/onboarding',
+    url: 'https://github.com/fastlane/onboarding'
+  },
+];
+
+export const mockRepositoryList =
+mockRepositoryListResponse.map((response) => new Repository(response));
+export const mockProjectResponse: RepositoryResponse = {
+  full_name: 'fastlane/ci',
+  api_path: 'https://api.github.com/fastlane/ci',
+  url: 'https://github.com/fastlane/ci'
+};
+
+export const mockRepository = new Repository(mockProjectResponse);
