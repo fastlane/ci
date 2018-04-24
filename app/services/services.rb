4dd61704830e44ef1a3c5b6334ba7489f3de7aff
Felix Krause <krausefx@gmail.com>
2018-04-24 10:09:09 +0200

    Fix code style

--- app/services/services.rb
+++ app/services/services.rb
@@ -191,7 +191,9 @@ module FastlaneCI
     end
 
     def self.onboarding_user_client
-      @_onboarding_user_client ||= Octokit::Client.new(access_token: FastlaneCI.dot_keys.initial_onboarding_user_api_token)
+      @_onboarding_user_client ||= Octokit::Client.new(
+        access_token: FastlaneCI.dot_keys.initial_onboarding_user_api_token
+      )
     end
   end
 end
