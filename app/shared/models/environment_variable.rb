23d1598637056eaacb1db69b681b3795bee689ba
Felix Krause <krausefx@gmail.com>
2018-04-25 10:47:54 +0200

    Code style fixes

--- app/shared/models/environment_variable.rb
+++ app/shared/models/environment_variable.rb
@@ -4,8 +4,6 @@ module FastlaneCI
   # EnvironmentVariable model representing a single entry of an env variable
   class EnvironmentVariable
     attr_accessor :key
-    attr_accessor :value
-    attr_reader :encrypted_value
 
     def initialize(key: nil, value: nil)
       @key = key
