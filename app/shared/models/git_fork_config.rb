f476c973a313e3aad352bd8d661ed8ea6ba99584
Felix Krause <krausefx@gmail.com>
2018-05-04 12:00:37 +0200

    Add documentation for GitForkConfig

--- app/shared/models/git_fork_config.rb
+++ app/shared/models/git_fork_config.rb
@@ -1,9 +1,18 @@
 module FastlaneCI
   # Information that is required for us to be able to checkout a branch/sha from a fork
+  # This information is also stored together with `FastlaneCI::Build`
   class GitForkConfig
+    # @return [String]
     attr_reader :clone_url
+
+    # @return [String] e.g. 
     attr_reader :branch
-    attr_reader :ref # sometimes we have a ref we can use, in that case, we don't need to pull a fork
+
+    # @return [String] e.g. "new-topic", "pull/123/head"
+    #   sometimes we have a ref we can use, in that case, we don't need to pull a fork
+    attr_reader :ref 
+
+    # @return [String]
     attr_reader :current_sha
 
     # If you have a ref you can pass, e.g.: `pull/661/head`, that's preferred
