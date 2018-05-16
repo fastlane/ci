21bb5ab07519eaa3e38442df9a628aa820fa84e5
Jorge Revuelta <jrevuelta@usal.es>
2018-05-16 02:01:02 +0200

    Added lots of tests

--- app/features-json/middleware/jwt_auth.rb
+++ app/features-json/middleware/jwt_auth.rb
@@ -5,26 +5,30 @@ require "jwt"
 module FastlaneCI
   # API Middleware responsible of authenticate all the requests that uses it.
   class JwtAuth
-    def initialize(app)
+    def initialize(app, encryption_key=nil)
       @app = app
+      @encryption_key = encryption_key || FastlaneCI.dot_keys.encryption_key
     end
 
     def call(env)
-      options = { algorithm: "HS256", iss: "fastlane.ci" }
+      options = { verify_iss: true, verify_iat: true, algorithm: "HS256", iss: "fastlane.ci" }
       bearer = env.fetch("HTTP_AUTHORIZATION", "").slice(7..-1)
-      payload, = JWT.decode(bearer, FastlaneCI.dot_keys.encryption_key, true, options)
+      payload = JWT.decode(bearer, @encryption_key, true, options)
 
+      raise "Missing user ID in payload." unless payload["user"]
       env[:user] = payload["user"]
 
       @app.call(env)
-    rescue JWT::DecodeError
-      [401, { "Content-Type" => "text/plain" }, ["A token must be passed."]]
-    rescue JWT::ExpiredSignature
-      [403, { "Content-Type" => "text/plain" }, ["The token has expired."]]
     rescue JWT::InvalidIssuerError
       [403, { "Content-Type" => "text/plain" }, ["The token does not have a valid issuer."]]
     rescue JWT::InvalidIatError
       [403, { "Content-Type" => "text/plain" }, ['The token does not have a valid "issued at" time.']]
+    rescue JWT::ExpiredSignature
+      [401, { "Content-Type" => "text/plain" }, ["The token has expired."]]
+    rescue JWT::DecodeError
+      [401, { "Content-Type" => "text/plain" }, ["A token must be passed."]]
+    rescue StandardError => ex
+      [500, { "Content-Type" => "text/plain" }, [ex.to_s]]
     end
   end
 end
--- spec/features-json/jwt_auth_spec.rb
+++ spec/features-json/jwt_auth_spec.rb
@@ -1,26 +1,73 @@
+require "jwt"
+
 require "spec_helper"
 require "app/features-json/middleware/jwt_auth"
 
 describe FastlaneCI::JwtAuth do
-  let(:app) { ->(env) { [200, env, "app"] } }
-  let(:middleware) { described_class.new(app) }
+  let(:inner_app) { ->(env) { [200, env, "app"] } }
+  let(:app) { described_class.new(inner_app, "fastlane-ci-test") }
 
   context "Client makes a request without authentication headers" do
-    let(:request) { Rack::MockRequest.new(middleware) }
-    let(:response) { request.get("/buy/tacos") }
 
     it "Returns a 401 status" do
-      expect(response.status).to eql(401)
+      get("/buy/tacos")
+      expect(last_response.status).to eql(401)
+      expect(last_response.body).to eql("A token must be passed.")
     end
   end
 
   context "Client makes a request with an expired authentication token" do
-    let(:request) { Rack::MockRequest.new(middleware) }
-    let(:expired_header) { { "HTTP_AUTHORIZATION": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjExNjM5MjYxLCJpYXQiOjExNjM5MjYxLCJpc3MiOiJmYXN0bGFuZS5jaSJ9._wzA6VzEuex1wJZctYHk94lCDMydOIe7scENvsCqTes" } }
-    let(:response) { request.get("/buy/tacos", expired_header) }
+    let(:data) { { user: "fastlane", iat: Time.at(0), exp: Time.at(0), iss: "fastlane.ci" } }
+    let(:token) { JWT.encode(data, "fastlane-ci-test", "HS256") }
+    let(:expired_authorization) { "Bearer #{token}" }
+
+    it "Returns a 401 status" do
+      header("Authorization", expired_authorization)
+      get("/buy/tacos")
+
+      expect(last_response.status).to eql(401)
+      expect(last_response.body).to eql("The token has expired.")
+    end
+  end
+
+  context "Client makes a request with an invalid issued at time" do
+    let(:data) { { user: "fastlane", iat: "an invalid iat", iss: "fastlane.ci" } }
+    let(:token) { JWT.encode(data, "fastlane-ci-test", "HS256") }
+    let(:invalid_authorization) { "Bearer #{token}" }
 
     it "Returns a 403 status" do
-      expect(response.status).to eql(403)
+      header("Authorization", invalid_authorization)
+      get("/buy/tacos")
+
+      expect(last_response.status).to eql(403)
+      expect(last_response.body).to eql('The token does not have a valid "issued at" time.')
+    end
+  end
+
+  context "Client makes a request with an invalid issuer" do
+    let(:data) { { user: "fastlane", iat: Time.now.to_i, exp: Time.now.to_i + 60 } }
+    let(:token) { JWT.encode(data, "fastlane-ci-test", "HS256") }
+    let(:invalid_issuer) { "Bearer #{token}" }
+
+    it "Returns a 403 status" do
+      header("Authorization", invalid_issuer)
+      get("/buy/tacos")
+
+      expect(last_response.status).to eql(403)
+      expect(last_response.body).to eql("The token does not have a valid issuer.")
+    end
+  end
+
+  context "Client makes a request with missing payload" do
+    let(:data) { { iat: Time.now.to_i, exp: Time.now.to_i + 60, iss: "fastlane.ci" } }
+    let(:token) { JWT.encode(data, "fastlane-ci-test", "HS256") }
+    let(:invalid_data) { "Bearer #{token}" }
+
+    it "Returns a 400 status" do
+      header("Authorization", invalid_data)
+      get("/buy/tacos")
+
+      expect(last_response.status).to eql(500)
     end
   end
 end
