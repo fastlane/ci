0cc87c60a96b4817410a9d56c5ecdd145e7bf2e9
Jorge <minuscorp@gmail.com>
2018-05-16 18:17:50 +0200

    change bang operator for halt server (internal server error)

--- app/features-json/json_authenticated_controller_base.rb
+++ app/features-json/json_authenticated_controller_base.rb
@@ -13,31 +13,30 @@ module FastlaneCI
     include FastlaneCI::Logging
 
     # Method to obtain the current user given their primary key.
-    # Raises if no User found.
     # @return [User]
-    def user!
+    def current_user
       user = FastlaneCI::Services.user_service.find_user(id: env[:user])
       if user.nil?
-        raise "No user found with ID #{env[:user]}"
+        halt 500
       else
         return user
       end
     end
 
     def current_user_config_service
-      @user_config_service = FastlaneCI::ConfigService.new(ci_user: user!)
+      @user_config_service = FastlaneCI::ConfigService.new(ci_user: current_user)
       return @user_config_service
     end
 
     def check_and_get_provider_credential(type: FastlaneCI::ProviderCredential::PROVIDER_CREDENTIAL_TYPES[:github])
-      provider_credential = user!.provider_credential(type: type)
-      raise "User #{user!.email} doesn't have any linked `#{type}` accounts" if provider_credential.nil?
+      provider_credential = current_user.provider_credential(type: type)
+      raise "User #{current_user.email} doesn't have any linked `#{type}` accounts" if provider_credential.nil?
       return provider_credential
     end
 
     def user_project_with_id(project_id: nil)
       project = FastlaneCI::Services.project_service.project_by_id(project_id)
-      raise "User #{user!.email} doesn't have access to a project with id `#{project_id}`" if project.nil?
+      raise "User #{current_user.email} doesn't have access to a project with id `#{project_id}`" if project.nil?
       return project
     end
   end
