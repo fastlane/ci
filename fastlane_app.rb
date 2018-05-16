1a25394c1d65c8828559c75570ccf368994f7926
Jorge <minuscorp@gmail.com>
2018-05-16 23:41:38 +0200

    Use JWTAuth Middleware to check correct authentication with the backend. (#872)
    * Use JWTAuth Middleware to check correct authentication with the backend.
    
    * Added tests
    
    * Added lots of tests
    
    * change bang operator for halt server (internal server error)
    
    * typo

--- fastlane_app.rb
+++ fastlane_app.rb
@@ -12,7 +12,6 @@ require_relative "app/shared/logging_module"
 require_relative "app/shared/dot_keys_variables"
 require_relative "app/shared/fastlane_ci_error" # TODO: move somewhere else
 require_relative "app/features/build_runner/build_runner"
-require_relative "app/features-json/jwt_auth"
 
 # All things fastlane ci related go in this module
 module FastlaneCI
