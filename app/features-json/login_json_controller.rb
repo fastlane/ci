2bdd5f5f7ebd8ec6d1dd38032c0c8df829cda250
Stefan Natchev <snatchev@google.com>
2018-05-23 16:17:48 -0400

    migrate api controllers to use APIController

--- app/features-json/login_json_controller.rb
+++ app/features-json/login_json_controller.rb
@@ -1,33 +1,25 @@
-require_relative "../shared/controller_base"
 require_relative "../services/user_service"
-require_relative "../services/dot_keys_variable_service"
+require_relative "api_controller"
 require_relative "json_params"
-
-require "jwt"
-require "json"
 
 module FastlaneCI
   # Controller responsible of handling the login process using JWT token.
-  class LoginJSONController < ControllerBase
-    include JSONParams
+  class LoginJSONController < APIController
+    disable(:authentication)
 
     HOME = "/api/login"
 
     post HOME.to_s do
-      # Allow this endpoint to be requested with { Content-Type: application/json }
       user = Services.user_service.login(email: params[:email], password: params[:password])
-      if user.nil?
-        halt(401)
-      else
-        content_type(:json)
-        { token: token(user) }.to_json
-      end
+      halt(401) unless user
+
+      json({ token: token(user) })
     end
 
     private
 
     def token(user)
-      JWT.encode(payload(user), FastlaneCI.dot_keys.encryption_key, "HS256")
+      JWT.encode(payload(user), settings.jwt_secret, settings.jwt_algo)
     end
 
     def payload(user)
