a49f71a1370509c0e1fab0aec6ebd91a4fd29090
Nima Akhbari <nakhbari@users.noreply.github.com>
2018-05-25 12:26:40 -0700

    Get build details from backend (#936)

--- app/features-json/build_json_controller.rb
+++ app/features-json/build_json_controller.rb
@@ -4,13 +4,20 @@ require_relative "./view_models/build_view_model"
 module FastlaneCI
   # Controller for providing all data relating to builds
   class BuildJSONController < APIController
-    HOME = "/data/project/:project_id/build"
+    HOME = "/data/projects/:project_id/build"
 
     get "#{HOME}/:build_number" do |project_id, build_number|
       build = current_project.builds.find { |b| b.number == build_number.to_i }
       build_view_model = BuildViewModel.new(build: build)
 
       json(build_view_model)
+    end
+
+    def current_project
+      current_project = FastlaneCI::Services.project_service.project_by_id(params[:project_id])
+      halt(404) unless current_project
+
+      return current_project
     end
   end
 end
