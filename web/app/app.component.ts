b9c1df39a50a56db9830e471c7d860915ab9166d
Nima Akhbari <nakhbari@users.noreply.github.com>
2018-05-25 12:29:10 -0700

    Add build page details and placeholders for the logs (#937)

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
 
 @Component({selector: 'fci-root', templateUrl: './app.component.html'})
 export class AppComponent {
+  @HostBinding('class') classes = ['fci-full-height-container'];
 }
--- web/app/build/build.component.ts
+++ web/app/build/build.component.ts
@@ -1,10 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ParamMap} from '@angular/router/src/shared';
 import {Subject} from 'rxjs/Subject';
 
 import {Breadcrumb} from '../common/components/toolbar/toolbar.component';
+import {BuildStatus} from '../common/constants';
+import {Build} from '../models/build';
 import {BuildLogWebsocketService} from '../services/build-log-websocket.service';
+import {DataService} from '../services/data.service';
 
 @Component({
   selector: 'fci-build',
@@ -12,30 +15,51 @@ import {BuildLogWebsocketService} from '../services/build-log-websocket.service'
   styleUrls: ['./build.component.scss']
 })
 export class BuildComponent implements OnInit {
-  logs = '';
+  @HostBinding('class') classes = ['fci-full-height-container'];
+  build: Build;
+  // TODO: define interface for the logs
+  logs: string[] = [];
+  readonly BuildStatus = BuildStatus;
+
   readonly breadcrumbs: Breadcrumb[] =
       [{label: 'Dashboard', url: '/'}, {hint: 'Project'}, {hint: 'Build'}];
 
   constructor(
+      private readonly dataService: DataService,
       private readonly buildLogSocketService: BuildLogWebsocketService,
       private readonly route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.paramMap
         .switchMap((params: ParamMap) => {
-          this.updateBreadcrumbs(params.get('projectId'));
-          return this.buildLogSocketService.connect(
-              params.get('projectId'), params.get('buildId'));
+          const projectId = params.get('projectId');
+          const buildNumber = +params.get('buildId');
+          this.updateBreadcrumbsLink(projectId);
+          this.connectLogSocket(projectId, buildNumber);
+          return this.dataService.getBuild(projectId, buildNumber);
         })
-        .subscribe((message) => {
-          // TODO: this will be more than a string later on. Let's define a log
-          // line model.
-          this.logs += message.data;
+        .subscribe((build: Build) => {
+          this.build = build;
+          this.updateBreadcrumbsLabels(build.projectId, build.number);
         });
   }
 
-  private updateBreadcrumbs(projectId: string): void {
-    // TODO: update the breadcrumbs to have Project name and Build number
+  private connectLogSocket(projectId: string, buildNumber: number) {
+    this.buildLogSocketService.connect(projectId, buildNumber)
+        .subscribe((message) => {
+          // TODO: define a log line model.
+          this.logs.push(message.data);
+        });
+  }
+
+  private updateBreadcrumbsLink(projectId: string): void {
     this.breadcrumbs[1].url = `/project/${projectId}`;
+  }
+
+  private updateBreadcrumbsLabels(projectName: string, buildNumber: number):
+      void {
+    // TODO: get project name from backend
+    this.breadcrumbs[1].label = 'Project';
+    this.breadcrumbs[2].label = `Build ${buildNumber}`;
   }
 }
