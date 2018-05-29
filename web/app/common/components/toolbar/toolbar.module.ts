37db4dcb0fe4093e7eadb8a3b0cd0586e2ed0be0
Nima Akhbari <nakhbari@users.noreply.github.com>
2018-05-29 08:52:09 -0700

    Style breadcrumbs to look 💣 (#938)

--- web/app/common/components/toolbar/toolbar.module.ts
+++ web/app/common/components/toolbar/toolbar.module.ts
@@ -1,12 +1,20 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatIconModule} from '@angular/material';
 import {RouterModule} from '@angular/router';
 
 import {ToolbarComponent} from './toolbar.component';
 
 @NgModule({
   declarations: [ToolbarComponent],
-  imports: [RouterModule, CommonModule],
+  imports: [
+    /** Angular Library Imports */
+    CommonModule, RouterModule,
+    /** Internal Imports */
+    /** Angular Material Imports */
+    MatIconModule
+    /** Third-Party Module Imports */
+  ],
   exports: [ToolbarComponent]
 })
 export class ToolbarModule {
