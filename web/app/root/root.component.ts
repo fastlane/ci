42f6ef71c673bc2b3104a10356c04dfa41b301f2
Nima Akhbari <nakhbari@users.noreply.github.com>
2018-05-31 09:16:54 -0700

    Move app to root/ folder (#946)

--- web/app/root/root.component.ts
+++ web/app/root/root.component.ts
@@ -1,6 +1,6 @@
 import {Component, HostBinding} from '@angular/core';
 
-@Component({selector: 'fci-root', templateUrl: './app.component.html'})
-export class AppComponent {
+@Component({selector: 'fci-root', templateUrl: './root.component.html'})
+export class RootComponent {
   @HostBinding('class') classes = ['fci-full-height-container'];
 }
