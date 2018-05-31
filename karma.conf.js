bce14036ba1bb1111ec01bbb6f4329aefa9ecceb
Jorge Revuelta <jrevuelta@usal.es>
2018-05-31 13:16:44 +0200

    :rocket:

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,3 @@
-var argv = require('yargs').argv;
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
@@ -38,7 +37,6 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: !argv.watch,
+    browsers: ['Chrome']
   });
 };
