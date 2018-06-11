096a14fd694302e6466a865f260bd9c8f9b36abb
Irfan Ahmad <irfan.ahmad@upgrad.com>
2018-06-11 14:14:35 +0530

    corrected reference for shared_material.module

--- web/app/common/components/common-components.module.ts
+++ web/app/common/components/common-components.module.ts
@@ -1,7 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
-import {SharedMaterialModule} from '../../shared_material.module';
+import {SharedMaterialModule} from '../../root/shared_material.module';
 
 import {StatusIconComponent} from './status-icon/status-icon.component';
 
