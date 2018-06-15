5258df8614f68575f34d5db41db9de49560aaf78
bogdanbrato <bogdanbrato@gmail.com>
2018-06-15 11:17:40 -0700

    Really remove spawn

--- agent/service.rb
+++ agent/service.rb
@@ -27,30 +27,6 @@ module FastlaneCI
         @busy
       end
 
-      ##
-      # spawns a command using popen2e. Merging stdout and stderr,
-      # because its easiest to return the lazy stream when both stdout and stderr pipes are together.
-      # otherwise, we run the risk of deadlock if we dont properly flush both pipes as per:
-      # https://ruby-doc.org/stdlib-2.1.0/libdoc/open3/rdoc/Open3.html#method-c-popen3
-      #
-      # @input FastlaneCI::Agent::Command
-      # @output Enumerable::Lazy<FastlaneCI::Agent::Log> A lazy enumerable with log lines.
-      def spawn(command, _call)
-        logger.info("spawning process with command: #{command.bin} #{command.parameters}, env: #{command.env.to_h}")
-        stdin, stdouterr, wait_thrd = Open3.popen2e(command.env.to_h, command.bin, *command.parameters)
-        stdin.close
-
-        logger.info("spawned process with pid: #{wait_thrd.pid}")
-
-        output_enumerator = ProcessOutputEnumerator.new(stdouterr, wait_thrd)
-        # convert every line from io to a Log object in a lazy stream
-        output_enumerator.lazy.flat_map do |line, status|
-          # proto3 doesn't have nullable fields, afaik
-          log = FastlaneCI::Proto::Log.new(message: (line || NULL_CHAR), status: (status || 0))
-          FastlaneCI::Proto::InvocationResponse.new(log: log)
-        end
-      end
-
       def run_fastlane(invocation_request, _call)
         command = invocation_request.command
         logger.info("RPC run_fastlane: #{command.bin} #{command.parameters}, env: #{command.env.to_h}")
