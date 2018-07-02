6d3233e44d4d95ded7c79804d0ec1c447f134286
Nima Akhbari <nnakhbari@gmail.com>
2018-07-02 15:15:50 -0700

    Add encryption key and form

--- web/app/onboard/onboard.component.spec.ts
+++ web/app/onboard/onboard.component.spec.ts
@@ -1,9 +1,13 @@
 import {DOCUMENT} from '@angular/common';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule, MatStepperModule} from '@angular/material';
 import {By} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {OnboardComponent} from './onboard.component';
+
+const FORM_CONTROL_IDS: string[] = ['encryptionKey'];
 
 describe('OnboardComponent', () => {
   let fixture: ComponentFixture<OnboardComponent>;
@@ -15,7 +19,10 @@ describe('OnboardComponent', () => {
 
     TestBed
         .configureTestingModule({
-          imports: [MatStepperModule, MatButtonModule],
+          imports: [
+            MatStepperModule, BrowserAnimationsModule, MatButtonModule,
+            ReactiveFormsModule
+          ],
           declarations: [
             OnboardComponent,
           ],
@@ -24,7 +31,28 @@ describe('OnboardComponent', () => {
 
     fixture = TestBed.createComponent(OnboardComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  for (const control_id of FORM_CONTROL_IDS) {
+    it(`should have the ${control_id} control properly attached`, () => {
+      const controlEl: HTMLInputElement =
+          fixture.debugElement
+              .query(By.css(`input[formcontrolname="${control_id}"]`))
+              .nativeElement;
+
+      controlEl.value = '10';
+      controlEl.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      expect(component.form.get(control_id).value).toBe('10');
+
+      component.form.patchValue({[control_id]: '12'});
+      fixture.detectChanges();
+
+      expect(controlEl.value).toBe('12');
+    });
+  }
 
   it('should redirect to old onboarding when button is clicked', () => {
     spyOn(component, 'goToOldOnboarding');
--- web/app/onboard/onboard.component.ts
+++ web/app/onboard/onboard.component.ts
@@ -1,14 +1,28 @@
 import {DOCUMENT} from '@angular/common';
 import {Component, Inject} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+
+function buildProjectForm(fb: FormBuilder): FormGroup {
+  return fb.group({
+    'encryptionKey': ['', Validators.required],
+  });
+}
 
 @Component({
   selector: 'fci-onboard',
   templateUrl: './onboard.component.html',
   styleUrls: ['./onboard.component.scss']
 })
-
 export class OnboardComponent {
-  constructor(@Inject(DOCUMENT) private readonly document: any) {}
+  readonly form: FormGroup;
+
+  constructor(
+      @Inject(DOCUMENT) private readonly document: any,
+      fb: FormBuilder,
+  ) {
+    this.form = buildProjectForm(fb);
+  }
 
   goToOldOnboarding() {
     this.document.location.href =
