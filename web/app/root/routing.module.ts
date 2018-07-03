2811af2da51a27d5303851bbe44116c1ffcb00a7
Nima Akhbari <nakhbari@users.noreply.github.com>
2018-07-03 11:46:42 -0700

    Add Initial Sign up page (#1049)
    * Recovery of metadata api call that i mistakenly overwrote
    
    * testing out a stateless user details check
    
    * Add Initial Sign up page
    
    * Fix tests

--- web/app/root/routing.module.ts
+++ web/app/root/routing.module.ts
@@ -11,6 +11,8 @@ import {OnboardComponent} from '../onboard/onboard.component';
 import {OnboardModule} from '../onboard/onboard.module';
 import {ProjectComponent} from '../project/project.component';
 import {ProjectModule} from '../project/project.module';
+import {SignupComponent} from '../signup/signup.component';
+import {SignupModule} from '../signup/signup.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
@@ -18,12 +20,13 @@ const routes: Routes = [
   {path: 'project/:id', component: ProjectComponent},
   {path: 'project/:projectId/build/:buildId', component: BuildComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
   {path: 'onboard', component: OnboardComponent},
 ];
 
 @NgModule({
   imports: [
-    DashboardModule, ProjectModule, BuildModule, LoginModule, OnboardModule,
+    DashboardModule, ProjectModule, BuildModule, LoginModule, SignupModule, OnboardModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
