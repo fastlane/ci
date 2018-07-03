3f223a4e76e8a1c88d13139bd3281f4ef7d38d6b
Nima Akhbari <nnakhbari@gmail.com>
2018-07-03 14:44:56 -0700

    Add fastlane bot configuration to onboarding

--- web/app/onboard/onboard.component.ts
+++ web/app/onboard/onboard.component.ts
@@ -2,10 +2,15 @@ import {DOCUMENT} from '@angular/common';
 import {Component, Inject} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+import {GITHUB_API_TOKEN_LENGTH} from '../common/constants';
+import {DataService} from '../services/data.service';
+
 
 function buildProjectForm(fb: FormBuilder): FormGroup {
   return fb.group({
     'encryptionKey': ['', Validators.required],
+    'botToken': ['', Validators.required],
+    'botPassword': ['', Validators.required],
   });
 }
 
@@ -16,12 +21,27 @@ function buildProjectForm(fb: FormBuilder): FormGroup {
 })
 export class OnboardComponent {
   readonly form: FormGroup;
+  botEmail: string;
+  isFetchingBotEmail = false;
 
   constructor(
       @Inject(DOCUMENT) private readonly document: any,
+      private readonly dataService: DataService,
       fb: FormBuilder,
   ) {
     this.form = buildProjectForm(fb);
+
+    this.form.get('botToken').valueChanges.subscribe((token) => {
+      delete this.botEmail;
+      if (token.length === GITHUB_API_TOKEN_LENGTH) {
+        this.isFetchingBotEmail = true;
+
+        this.dataService.getUserDetails(token).subscribe((details) => {
+          this.botEmail = details.github.email;
+          this.isFetchingBotEmail = false;
+        });
+      }
+    });
   }
 
   goToOldOnboarding() {
