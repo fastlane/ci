875c85acc71ee690519d308dc1dab5cd44b1d571
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2018-07-06 23:34:29 -0400

    Added specs for `CheckForNewPullRequestsOnGithubWorker` and updated specs for `CheckForNewCommitsOnGithubWorker`.

--- spec/workers/check_for_new_commits_on_github_worker_spec.rb
+++ spec/workers/check_for_new_commits_on_github_worker_spec.rb
@@ -40,7 +40,10 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
   end
 
   # Shas for commits that have previously had builds run for them.
-  let(:master_prev_run_sha) { "acdacfe6-af6e-4190-a627-f85ea4cfa769" }
+  let(:master_prev_run_sha_one) { "acdacfe6-af6e-4190-a627-f85ea4cfa769" }
+  let(:master_prev_run_sha_two) { "84793bab-80b8-4141-9398-804a72c73851" }
+  let(:patch_1_prev_run_sha_one) { "76023ce3-aaf5-4b31-922f-78086a91095b" }
+  let(:patch_1_prev_run_sha_two) { "47ef6f6a-c49d-467b-9c86-620ff1aecadd" }
   let(:patch_2_prev_run_sha_one) { "e51c9638-33b2-4baa-8128-8579809fff16" }
   let(:patch_2_prev_run_sha_two) { "30a9f509-695d-4ce7-8c24-7a71f0c9341a" }
 
@@ -52,10 +55,10 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
   # Test doubles representing `FastlaneCI::Build`s.
   let(:build_doubles) do
     [
-      double("build_0", branch: "master", sha: master_prev_run_sha),
-      double("build_1", branch: "master", sha: "84793bab-80b8-4141-9398-804a72c73851"),
-      double("build_2", branch: "patch-1", sha: "76023ce3-aaf5-4b31-922f-78086a91095b"),
-      double("build_3", branch: "patch-1", sha: "47ef6f6a-c49d-467b-9c86-620ff1aecadd"),
+      double("build_0", branch: "master", sha: master_prev_run_sha_one),
+      double("build_1", branch: "master", sha: master_prev_run_sha_two),
+      double("build_2", branch: "patch-1", sha: patch_1_prev_run_sha_one),
+      double("build_3", branch: "patch-1", sha: patch_1_prev_run_sha_two),
       double("build_4", branch: "patch-2", sha: patch_2_prev_run_sha_one),
       double("build_5", branch: "patch-2", sha: patch_2_prev_run_sha_two)
     ]
@@ -66,12 +69,15 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
     {
       # This branch `master`, one of the commits has been enqueued in a build.
       "master" => [
-        double("commit_0", sha: master_prev_run_sha),
+        double("commit_0", sha: master_prev_run_sha_one),
+        # Commit containing `sha` that needs to be enqueued in a build.
         double("commit_1", sha: master_unrun_sha)
       ],
       # This branch `patch-1`, none of the commits have been enqueued in builds.
       "patch-1" => [
+        # Commit containing `sha` that needs to be enqueued in a build.
         double("commit_2", sha: patch_1_unrun_sha_one),
+        # Commit containing `sha` that needs to be enqueued in a build.
         double("commit_3", sha: patch_1_unrun_sha_two)
       ],
       # This branch `patch-2`, all of the commits have been enqueued in builds,
@@ -118,7 +124,7 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
 
         # Expectations on hash values.
         expect(filtered_commit_shas).to include(master_unrun_sha)
-        expect(filtered_commit_shas).not_to include(master_prev_run_sha)
+        expect(filtered_commit_shas).not_to include(master_prev_run_sha_one)
 
         # Sanity check.
         expect(filtered_commit_shas).not_to include(patch_1_unrun_sha_one, patch_1_unrun_sha_two, patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
@@ -140,7 +146,7 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
         expect(filtered_commit_shas).to include(patch_1_unrun_sha_one, patch_1_unrun_sha_two)
 
         # Sanity check.
-        expect(filtered_commit_shas).not_to include(master_prev_run_sha, master_unrun_sha, patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
+        expect(filtered_commit_shas).not_to include(master_prev_run_sha_one, master_unrun_sha, patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
       end
     end
 
@@ -159,7 +165,7 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
         expect(filtered_commit_shas).not_to include(patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
 
         # Sanity check.
-        expect(filtered_commit_shas).not_to include(master_prev_run_sha, master_unrun_sha, patch_1_unrun_sha_one, patch_1_unrun_sha_two)
+        expect(filtered_commit_shas).not_to include(master_prev_run_sha_one, master_unrun_sha, patch_1_unrun_sha_one, patch_1_unrun_sha_two)
       end
     end
 
@@ -175,7 +181,74 @@ describe FastlaneCI::CheckForNewCommitsOnGithubWorker do
 
         # Expectations on hash values.
         expect(filtered_commit_shas).to include(master_unrun_sha, patch_1_unrun_sha_one, patch_1_unrun_sha_two)
-        expect(filtered_commit_shas).not_to include(master_prev_run_sha, patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
+        expect(filtered_commit_shas).not_to include(master_prev_run_sha_one, patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
+      end
+    end
+  end
+
+  describe "#enqueue_new_builds" do
+    before(:each) do
+      number_of_builds = branch_name_to_commits_doubles.values.flatten.size
+      service.should_receive(:create_and_queue_build_task).exactly(number_of_builds).times
+    end
+
+    it "enqueues all the new builds properly" do
+      service.send(:enqueue_new_builds, branch_name_to_commits_doubles)
+    end
+  end
+
+  describe "#branch_name_to_builds" do
+    context "`master` branch only" do
+      before(:each) { service.stub(:branches) { Set.new(%w[master]) } }
+
+      it "Properly creates the hash mapping of branch name to corresponding builds." do
+        branch_name_to_builds = service.send(:branch_name_to_builds)
+        filtered_commit_shas = branch_name_to_builds.values.flatten.map(&:sha)
+
+        # Expectations on hash keys.
+        expect(branch_name_to_builds).to have_key("master")
+        expect(branch_name_to_builds.keys).not_to include("patch-1", "patch-2")
+
+        # Expectations on hash values.
+        expect(filtered_commit_shas).to include(master_prev_run_sha_one, master_prev_run_sha_two)
+
+        # NOTE: This shouldn't mapping include the commit shas.
+        expect(filtered_commit_shas).not_to include(master_unrun_sha)
+      end
+    end
+
+    context "`patch-1` branch only" do
+      before(:each) { service.stub(:branches) { Set.new(%w[patch-1]) } }
+
+      it "Properly creates the hash mapping of branch name to corresponding builds." do
+        branch_name_to_builds = service.send(:branch_name_to_builds)
+        filtered_commit_shas = branch_name_to_builds.values.flatten.map(&:sha)
+
+        # Expectations on hash keys.
+        expect(branch_name_to_builds).to have_key("patch-1")
+        expect(branch_name_to_builds.keys).not_to include("master", "patch-2")
+
+        # Expectations on hash values.
+        expect(filtered_commit_shas).to include(patch_1_prev_run_sha_one, patch_1_prev_run_sha_two)
+
+        # NOTE: This shouldn't mapping include the commit shas.
+        expect(filtered_commit_shas).not_to include(patch_1_unrun_sha_one, patch_1_unrun_sha_two)
+      end
+    end
+
+    context "`patch-2` branch only" do
+      before(:each) { service.stub(:branches) { Set.new(%w[patch-2]) } }
+
+      it "Properly creates the hash mapping of branch name to corresponding builds." do
+        branch_name_to_builds = service.send(:branch_name_to_builds)
+        filtered_commit_shas = branch_name_to_builds.values.flatten.map(&:sha)
+
+        # Expectations on hash keys.
+        expect(branch_name_to_builds).to have_key("patch-2")
+        expect(branch_name_to_builds.keys).not_to include("master", "patch-1")
+
+        # Expectations on hash values.
+        expect(filtered_commit_shas).to include(patch_2_prev_run_sha_one, patch_2_prev_run_sha_two)
       end
     end
   end
