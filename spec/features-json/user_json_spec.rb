0a97364f868a94b8d0000c7a0377c1b31eee92f9
Felix Krause <krausefx@gmail.com>
2018-07-09 15:20:03 -0400

    Update tests to stub github client

--- spec/features-json/user_json_spec.rb
+++ spec/features-json/user_json_spec.rb
@@ -13,20 +13,30 @@ describe FastlaneCI::UserJSONController do
   end
 
   describe "/api/user" do
-    it "creates a new user and attach the provider credentials" do
-      email = "email@email.com"
-      allow(FastlaneCI::Services.user_service.user_data_source).to receive(:user_exist?).with({ email: email }).and_return(false)
+    let(:fake_email) { "email@email.com" }
+    before do
+      github_client = "github_client"
+      expect(Octokit::Client).to receive(:new).and_return(github_client)
 
-      post "/api/user", { github_token: "github_token", password: "password", email: email }.to_json, { "CONTENT_TYPE" => "application/json" }
+      email_entry = "email_entry"
+      expect(email_entry).to receive(:primary).and_return(true)
+      expect(email_entry).to receive(:email).and_return(fake_email)
+
+      expect(github_client).to receive(:emails).and_return([email_entry])
+    end
+
+    it "creates a new user and attach the provider credentials" do
+      allow(FastlaneCI::Services.user_service.user_data_source).to receive(:user_exist?).with({ email: fake_email }).and_return(false)
+
+      post "/api/user", { github_token: "github_token", password: "password" }.to_json, { "CONTENT_TYPE" => "application/json" }
       expect(last_response).to be_ok
       expect(json["status"]).to eq("success")
     end
 
     it "returns an error if the user already exists" do
-      email = "email@email.com"
-      allow(FastlaneCI::Services.user_service.user_data_source).to receive(:user_exist?).with({ email: email }).and_return(true)
+      allow(FastlaneCI::Services.user_service.user_data_source).to receive(:user_exist?).with({ email: fake_email }).and_return(true)
 
-      post "/api/user", { github_token: "github_token", password: "password", email: email }.to_json, { "CONTENT_TYPE" => "application/json" }
+      post "/api/user", { github_token: "github_token", password: "password" }.to_json, { "CONTENT_TYPE" => "application/json" }
       expect(last_response.status).to eq(400)
       expect(json["key"]).to eq("User.Error")
       expect(json["message"]).to eq("Error creating new user")
