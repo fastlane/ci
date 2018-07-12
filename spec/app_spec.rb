5f4ec5d29f892ebc1c48ef8175c2c66e761235a9
Felix Krause <KrauseFx@gmail.com>
2018-07-12 15:21:55 -0400

    Remove unused example spec

--- spec/app_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path("../spec_helper.rb", __FILE__)
-
-# describe "sinatra example" do
-#   it "should allow accessing the home page" do
-#     get "/"
-#     expect(last_response.status).to eq(302)
-#   end
-# end
