f117d223e176924f126686a8f0647bcc11a45313
Nima Akhbari <nnakhbari@gmail.com>
2018-07-26 11:43:02 -0700

    Remove login and signup, not needed for OAuth

--- web/app/root/routing.module.ts
+++ web/app/root/routing.module.ts
@@ -5,28 +5,22 @@ import {BuildComponent} from '../build/build.component';
 import {BuildModule} from '../build/build.module';
 import {DashboardComponent} from '../dashboard/dashboard.component';
 import {DashboardModule} from '../dashboard/dashboard.module';
-import {LoginComponent} from '../login/login.component';
-import {LoginModule} from '../login/login.module';
 import {OnboardComponent} from '../onboard/onboard.component';
 import {OnboardModule} from '../onboard/onboard.module';
 import {ProjectComponent} from '../project/project.component';
 import {ProjectModule} from '../project/project.module';
-import {SignupComponent} from '../signup/signup.component';
-import {SignupModule} from '../signup/signup.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'project/:id', component: ProjectComponent},
   {path: 'project/:projectId/build/:buildId', component: BuildComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent},
   {path: 'onboard', component: OnboardComponent},
 ];
 
 @NgModule({
   imports: [
-    DashboardModule, ProjectModule, BuildModule, LoginModule, SignupModule, OnboardModule,
+    DashboardModule, ProjectModule, BuildModule, OnboardModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
